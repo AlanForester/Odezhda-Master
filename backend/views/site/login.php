42e2bc98274048a733c8b20e6c23ff666a16cb0e
Oleg <olegblud@gmail.com>
2014-01-19 01:01:10 +0200

    main fix

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -5,7 +5,7 @@
  */
 
 
-Yii::app()->getClientScript()->registerCssFile($this->assets_backend.'/login.css');
+Yii::app()->getClientScript()->registerCssFile($this->assets_backend . '/login.css');
 //Yii::app()->getClientScript()->registerCssFile(Yii::app()->request->baseUrl.'/adminlogin.css');
 $this->pageTitle = 'Вход';
 //$this->breadcrumbs = ['Login'];
@@ -16,39 +16,40 @@ $this->pageTitle = 'Вход';
 <!-- Login Form BEGIN -->
 <div class="form">
 
-<?php
-/** @var TbActiveForm $form */
-$form = $this->beginWidget(
-	'bootstrap.widgets.TbActiveForm',
-	array(
-		'id' => 'loginForm',
-		'enableClientValidation' => true,
-		'htmlOptions' => ['class' => 'well offset4'],
-		'clientOptions' => array(
-			'validateOnSubmit'=>true,
-		),
-	)
-);
+    <?php
+    /** @var TbActiveForm $form */
+    $form = $this->beginWidget(
+        'bootstrap.widgets.TbActiveForm',
+        array(
+            'id' => 'loginForm',
+            'enableClientValidation' => true,
+            'htmlOptions' => ['class' => 'well offset4'],
+            'clientOptions' => array(
+                'validateOnSubmit' => true,
+            ),
+        )
+    );
 
-echo CHtml::errorSummary($model, null, null, array('class' => 'alert alert-error'));
-?>
+    echo CHtml::errorSummary($model, null, null, array('class' => 'alert alert-error'));
+//    echo TbHtml::alert(TbHtml::ALERT_COLOR_ERROR, '..');
+    ?>
 
-<!--	<p class="lead">Log In Here</p>-->
+    <!--	<p class="lead">Log In Here</p>-->
     <legend>Авторизация</legend>
 
-    <?= $form->textField($model, 'username', array('class'=>'span3', 'placeholder'=>'Логин', 'prepend'=>TbHtml::icon(TbHtml::ICON_USER), 'size'=>'60'));?>
-	<?= $form->passwordField($model, 'password', array('class'=>'span3', 'placeholder'=>'Пароль', 'prepend'=>TbHtml::icon(TbHtml::ICON_LOCK)));?>
+    <?= $form->textField($model, 'username', array('class' => 'span3', 'placeholder' => 'Логин', 'prepend' => TbHtml::icon(TbHtml::ICON_USER), 'size' => '60')); ?>
+    <?= $form->passwordField($model, 'password', array('class' => 'span3', 'placeholder' => 'Пароль', 'prepend' => TbHtml::icon(TbHtml::ICON_LOCK))); ?>
 
-	<?php if ($model->isCaptchaRequired()): ?>
-		<?php $this->widget('CCaptcha'); ?>
-		<?= $form->textField($model, 'verifyCode'); ?>
-	<?php endif; ?>
+    <?php if ($model->isCaptchaRequired()): ?>
+        <?php $this->widget('CCaptcha'); ?>
+        <?= $form->textField($model, 'verifyCode'); ?>
+    <?php endif; ?>
 
-<!--	<div class="form-actions">-->
-		<?php $this->widget('backend.widgets.bootstrap.TbButton', array('id'=>'logInButton', 'buttonType'=>'submit','type'=>'primary','label'=>'Вход', 'icon'=>'ok'));?>
-<!--	</div>-->
+    <!--	<div class="form-actions">-->
+    <?php $this->widget('backend.widgets.bootstrap.TbButton', array('id' => 'logInButton', 'buttonType' => 'submit', 'type' => 'primary', 'label' => 'Вход', 'icon' => 'ok')); ?>
+    <!--	</div>-->
 
-<?php $this->endWidget(); ?>
+    <?php $this->endWidget(); ?>
 
 </div>
 <!-- Login Form END -->
