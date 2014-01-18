4c6543900c0889da45a4deb8f7ae27d785c3287d
dmitri <dmitriy@maybeworks.com>
2014-01-19 01:44:44 +0200

    users form

--- backend/views/users/edit.php
+++ backend/views/users/edit.php
@@ -1,6 +1,6 @@
 <?php
 //print_r($model);exit;
-$this->pageTitle='Редактирование ['.$user['email_address'].']';
+$this->pageTitle='Редактирование ['.$model->email_address.']';
 $this->pageButton = [
     TbHtml::submitButton(
         'Сохранить',
@@ -41,27 +41,54 @@ $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
     'layout' => TbHtml::FORM_LAYOUT_HORIZONTAL,
 ));?>
 <fieldset>
-    <?php echo $form->textFieldControlGroup($model, 'id', array('value'=>$user['id'],'disabled' => true)); ?>
-    <?php echo $form->textFieldControlGroup($model, 'Группа', array('value'=>$user['groups_id'])); ?>
-    <?php echo $form->textFieldControlGroup($model, 'Имя', array('value'=>$user['firstname'])); ?>
-    <?php echo $form->textFieldControlGroup($model, 'Фамилия', array('value'=>$user['lastname'])); ?>
-    <?php echo $form->textFieldControlGroup($model, 'Email', array('value'=>$user['email_address'])); ?>
-    <?php echo $form->textFieldControlGroup($model, 'created', array('value'=>$user['created'], 'disabled' => true)); ?>
-    <?php echo $form->textFieldControlGroup($model, 'modified', array('value'=>$user['modified'], 'disabled' => true)); ?>
-    <?php echo $form->textFieldControlGroup($model, 'Последнее посещение', array('value'=>$user['logdate'], 'disabled' => true)); ?>
-    <?php echo $form->textFieldControlGroup($model, 'Кол-во авторизаций', array('value'=>$user['lognum'], 'disabled' => true)); ?>
+    <?php
+     //   if (!empty($model->id))
+            echo $form->textFieldControlGroup($model, 'id', array('value'=>$model->id,'disabled' => true));
+    ?>
+    <?php
+       // if (!empty($model->groups_id))
+            echo $form->textFieldControlGroup($model, 'Группа', array('value'=>$model->groups_id));
+    ?>
+    <?php
+     //   if (!empty($model->firstname))
+            echo $form->textFieldControlGroup($model, 'Имя', array('value'=>$model->firstname));
+    ?>
+    <?php
+        //if (!empty($model->lastname))
+            echo $form->textFieldControlGroup($model, 'Фамилия', array('value'=>$model->lastname));
+    ?>
+    <?php
+      //  if (!empty($model->email_address))
+            echo $form->textFieldControlGroup($model, 'Email', array('value'=>$model->email_address));
+    ?>
+    <?php
+        if (!empty($model->created))
+            echo $form->textFieldControlGroup($model, 'Дата создания', array('value'=>$model->created, 'disabled' => true));
+    ?>
+    <?php
+        if (!empty($model->modified))
+            echo $form->textFieldControlGroup($model, 'Дата изменения', array('value'=>$model->modified, 'disabled' => true));
+    ?>
+    <?php
+        if (!empty($model->logdate))
+            echo $form->textFieldControlGroup($model, 'Последний визит', array('value'=>$model->logdate, 'disabled' => true));
+    ?>
+    <?php
+        if (!empty($model->lognum))
+            echo $form->textFieldControlGroup($model, 'Количество авторизаций', array('value'=>$model->lognum, 'disabled' => true));
+    ?>
 
 </fieldset>
 <?php $this->endWidget(); ?>
 
 
 <?php
-$form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
-    'layout' => TbHtml::FORM_LAYOUT_HORIZONTAL,
-    //'class'=>'span5'
-));?>
-        <?php echo $form->textFieldControlGroup($model, 'created', array('value'=>$user['created'], 'disabled' => true)); ?>
-        <?php echo $form->textFieldControlGroup($model, 'modified', array('value'=>$user['modified'], 'disabled' => true)); ?>
-        <?php echo $form->textFieldControlGroup($model, 'Последнее посещение', array('value'=>$user['logdate'], 'disabled' => true)); ?>
-        <?php echo $form->textFieldControlGroup($model, 'Кол-во авторизаций', array('value'=>$user['lognum'], 'disabled' => true)); ?>
-<?php $this->endWidget(); ?>
+//$form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+//    'layout' => TbHtml::FORM_LAYOUT_HORIZONTAL,
+//    //'class'=>'span5'
+//));?>
+<!--        --><?php //echo $form->textFieldControlGroup($model, 'created', array('value'=>$user['created'], 'disabled' => true)); ?>
+<!--        --><?php //echo $form->textFieldControlGroup($model, 'modified', array('value'=>$user['modified'], 'disabled' => true)); ?>
+<!--        --><?php //echo $form->textFieldControlGroup($model, 'Последнее посещение', array('value'=>$user['logdate'], 'disabled' => true)); ?>
+<!--        --><?php //echo $form->textFieldControlGroup($model, 'Кол-во авторизаций', array('value'=>$user['lognum'], 'disabled' => true)); ?>
+<?php //$this->endWidget(); ?>
