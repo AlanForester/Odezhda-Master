0e2959ceb47bd5a3380da662fa2188db78141abb
Oleg <olegblud@gmail.com>
2014-01-19 01:50:23 +0200

    error page

--- backend/views/site/error.php
+++ backend/views/site/error.php
@@ -5,15 +5,21 @@
  * @var string $message
  */
 
-$this->pageTitle='Ошибка';
+$this->pageTitle = 'Ошибка';
 //$this->pageTitle=Yii::app()->name . ' - Ошибка';
 //$this->breadcrumbs=array(
 //	'Error',
 //);
 ?>
 
-<h2><?= $code; ?></h2>
 
-<div class="error">
-<?= CHtml::encode($message); ?>
+
+<div class="error row">
+    <span class="span6">
+        <h2><?= $code; ?></h2>
+        <?= CHtml::encode($message); ?>
+    </span>
+    <span class="span6">
+        <img src="<?= $this->assets_backend . '/images/minions.jpg'; ?>">
+    </span>
 </div>
