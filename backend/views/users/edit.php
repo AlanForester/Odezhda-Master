2112d8377b8f9d7531afd92ed763406cb1772bb7
Oleg <olegblud@gmail.com>
2014-01-19 05:36:00 +0200

    редактирование пользователя

--- backend/views/users/edit.php
+++ backend/views/users/edit.php
@@ -44,91 +44,48 @@ $this->pageButton = [
 ];
 ?>
 <div class="span6">
-<?php
-/**
- * @var TbActiveForm $form
- */
-$form = $this->beginWidget(
-    'bootstrap.widgets.TbActiveForm',
-    array(
-        'layout' => TbHtml::FORM_LAYOUT_HORIZONTAL,
-    )
-);?>
+    <?php
+    /**
+     * @var TbActiveForm $form
+     */
+    $form = $this->beginWidget(
+        'bootstrap.widgets.TbActiveForm',
+        [
+            'layout' => TbHtml::FORM_LAYOUT_HORIZONTAL,
+        ]
+    );?>
     <fieldset>
         <legend>Учетная запись</legend>
         <?php
-        //   if (!empty($model->id))
-//        echo $form->textFieldControlGroup($model, 'id', array('value' => $model->id, 'disabled' => true));
-        ?>
-        <?php
-        // if (!empty($model->groups_id))
-        echo $form->textFieldControlGroup($model, 'group', array('value' => $model->groups_id,'label'=>'Группа'));
-        ?>
-        <?php
-        //   if (!empty($model->firstname))
-        echo $form->textFieldControlGroup($model, 'firstname', array('value' => $model->firstname,'label'=>'Имя'));
-        ?>
-        <?php
-        //if (!empty($model->lastname))
-        echo $form->textFieldControlGroup($model, 'lastname', array('value' => $model->lastname,'label'=>'Фамилия'));
-        ?>
-        <?php
-        //  if (!empty($model->email_address))
-        echo $form->textFieldControlGroup($model, 'email_address', array('value' => $model->email_address,'label'=>'Email'));
-        ?>
-        <?php
-        echo $form->passwordFieldControlGroup($model, 'new_password', array('value' => '','label'=>'Новый пароль'));
-        //if (!empty($model->created))
-//            echo $form->textFieldControlGroup($model, 'Дата создания', array('value' => $model->created, 'disabled' => true));
-        ?>
-        <?php
-//        if (!empty($model->modified))
-//            echo $form->textFieldControlGroup($model, 'Дата изменения', array('value' => $model->modified, 'disabled' => true));
-        ?>
-        <?php
-//        if (!empty($model->logdate))
-//            echo $form->textFieldControlGroup($model, 'Последний визит', array('value' => $model->logdate, 'disabled' => true));
-        ?>
-        <?php
-
+        echo $form->textFieldControlGroup($model, 'group', array('value' => $model->groups_id, 'label' => 'Группа'));
+        echo $form->textFieldControlGroup($model, 'firstname', array('value' => $model->firstname, 'label' => 'Имя'));
+        echo $form->textFieldControlGroup($model, 'lastname', array('value' => $model->lastname, 'label' => 'Фамилия'));
+        echo $form->textFieldControlGroup($model, 'email_address', array('value' => $model->email_address, 'label' => 'Email'));
+        echo $form->passwordFieldControlGroup($model, 'new_password', array('value' => '', 'label' => 'Новый пароль'));
         ?>
 
     </fieldset>
     <input type="hidden" name="form_action" value="save">
-<?php $this->endWidget(); ?>
+    <?php $this->endWidget(); ?>
 </div>
 
 <div class="span6">
     <fieldset>
         <legend>Дополнительная информация</legend>
-    <?php
-    $this->widget(
-        'yiiwheels.widgets.detail.WhDetailView',
-        array(
-            'data' => $model,
-            'attributes' => array(
-                array('name' => 'id', 'label' => 'ID'),
-                array('name' => 'lognum', 'label' => 'Кол-во авторизаций'),
-                array('name' => 'logdate', 'label' => 'Последний визит'),
-                array('name' => 'modified', 'label' => 'Изменен'),
-                array('name' => 'created', 'label' => 'Создан'),
-            ),
-        ));
-
-//    if (!empty($model->lognum))
-//        echo $form->label($model, 'Количество авторизаций', array('value' => $model->lognum, 'disabled' => true));
-    ?>
+        <?php
+        $this->widget(
+            'yiiwheels.widgets.detail.WhDetailView',
+            array(
+                'data' => $model,
+                'attributes' => array(
+                    array('name' => 'id', 'label' => 'ID'),
+                    array('name' => 'lognum', 'label' => 'Кол-во авторизаций'),
+                    array('name' => 'logdate', 'label' => 'Последний визит'),
+                    array('name' => 'modified', 'label' => 'Изменен'),
+                    array('name' => 'created', 'label' => 'Создан'),
+                ),
+            )
+        );
+        ?>
     </fieldset>
 </div>
-
-<?php
-//$form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
-//    'layout' => TbHtml::FORM_LAYOUT_HORIZONTAL,
-//    //'class'=>'span5'
-//));
-?>
-    <!--        --><?php //echo $form->textFieldControlGroup($model, 'created', array('value'=>$user['created'], 'disabled' => true)); ?>
-    <!--        --><?php //echo $form->textFieldControlGroup($model, 'modified', array('value'=>$user['modified'], 'disabled' => true)); ?>
-    <!--        --><?php //echo $form->textFieldControlGroup($model, 'Последнее посещение', array('value'=>$user['logdate'], 'disabled' => true)); ?>
-    <!--        --><?php //echo $form->textFieldControlGroup($model, 'Кол-во авторизаций', array('value'=>$user['lognum'], 'disabled' => true)); ?>
-<?php //$this->endWidget(); ?>
