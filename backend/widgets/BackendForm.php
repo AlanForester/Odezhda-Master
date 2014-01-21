57ecd09655b00fe34a0c3eb83d0aea520f18d363
olegblud <olegblud@gmail.com>
2014-01-21 17:55:56 +0200

    виджет страницы, новая версия

---
+++ backend/widgets/BackendForm.php
@@ -0,0 +1,6 @@
+<?php
+
+class BackendForm extends CWidget {
+
+
+}
---
+++ backend/widgets/BackendList.php
@@ -0,0 +1,50 @@
+<?php
+
+class BackendList extends CWidget {
+
+    /**
+     * Кнопки управления для контроллера
+     * @var array
+     */
+    public $buttons = [];
+    //public $pageButton = [];
+
+    /**
+     * Элементы фильтра
+     * @var array
+     */
+    public $filter = [];
+
+    /**
+     * Контроллер страницы
+     * @var
+     */
+    public $controller;
+
+    public function init(){
+        if ($this->controller){
+            $this->controller->pageButton = $this->pageButton;
+        }
+    }
+
+    public function run(){
+
+        echo '
+            <div class="span2">
+                <div id="sidebar">'.$this->renderFilter().'</div>
+            </div>
+            <div class="span10">
+                <div>'.$this->renderGrid().'</div>
+            </div>
+        ';
+    }
+
+    public function renderFilter(){
+        return '<h4 class="page-header">Фильтр:</h4>';
+    }
+
+    public function renderGrid(){
+        return 'grid';
+    }
+
+}
