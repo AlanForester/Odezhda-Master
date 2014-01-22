fb77a07e51110026b796dfa55ca5fb07bc5768ad
olegblud <olegblud@gmail.com>
2014-01-22 13:48:53 +0200

    кнопки управления

--- backend/helpers/BackendPageButtons.php
+++ backend/helpers/BackendPageButtons.php
@@ -2,33 +2,33 @@
 
 class BackendPageButtons {
 
-
     public static function addButton($url = '', $option = [], $title = 'Добавить') {
         return
             TbHtml::linkButton(
-                $title,
-                array_merge(
-                    [
-                        'color' => TbHtml::BUTTON_COLOR_SUCCESS,
-                        'icon' => TbHtml::ICON_PLUS,
-                        'url' => Yii::app()->createUrl($url),
-                        'type' => 'success',
-                        'class' => 'btn-small'
-                    ], $option
-                )
+                  $title,
+                  array_merge(
+                      [
+                          'color' => TbHtml::BUTTON_COLOR_SUCCESS,
+                          'icon' => TbHtml::ICON_PLUS,
+                          'url' => Yii::app()
+                                      ->createUrl($url),
+                          'type' => 'success',
+                          'class' => 'btn-small'
+                      ], $option
+                  )
             );
     }
 
     public static function removeButton($url = '', $option = [], $title = 'Удалить') {
         return
             TbHtml::htmlButton(
-                $title,
-                array_merge(
-                    [
-                        'icon' => TbHtml::ICON_REMOVE,
-                        'url' => '#',
-                        'class' => 'btn-small',
-                        'onClick' => 'js: (function(){
+                  $title,
+                  array_merge(
+                      [
+                          'icon' => TbHtml::ICON_REMOVE,
+                          'url' => '#',
+                          'class' => 'btn-small',
+                          'onClick' => 'js: (function(){
                             var cb = $("input[name=\'gridids[]\']:checked");
                             var ids = [];
 
@@ -45,7 +45,8 @@ class BackendPageButtons {
                                             $.fn.yiiGridView.update(
                                                 "usersgrid",
                                                 {
-                                                    url:"' . Yii::app()->createUrl($url) . '",
+                                                    url:"' . Yii::app()
+                                                                ->createUrl($url) . '",
                                                     data:{
                                                         mass_action:"delete",
                                                         ids:ids
@@ -57,21 +58,21 @@ class BackendPageButtons {
                                 );
                             }
                         })()'
-                    ], $option
-                )
+                      ], $option
+                  )
             );
     }
 
     public static function massButton($url = '', $option = [], $title = 'Пакетная обработка') {
         return
             TbHtml::htmlButton(
-                $title,
-                array_merge(
-                    [
-                        'icon' => TbHtml::ICON_TASKS,
-                        'url' => '#',
-                        'class' => 'btn-small',
-                        'onClick' => 'js: (function(){
+                  $title,
+                  array_merge(
+                      [
+                          'icon' => TbHtml::ICON_TASKS,
+                          'url' => '#',
+                          'class' => 'btn-small',
+                          'onClick' => 'js: (function(){
                             var cb = $("input[name=\'gridids[]\']:checked");
                             if (cb.length==0){
                                 bootbox.alert({message:"Ввыберите минимум один обьект в списке",title:"Ошибка"});
@@ -79,8 +80,28 @@ class BackendPageButtons {
                                 bootbox.alert({message:"Пакетная обработка еще не реализована",title:"Ошибка"});
                             }
                         })()'
-                    ], $option
-                )
+                      ], $option
+                  )
+            );
+    }
+
+    public static function saveButton($option = [], $title = 'Сохранить') {
+        return
+            TbHtml::htmlButton(
+                  $title,
+                  array_merge([
+                                  'icon' => TbHtml::ICON_PENCIL,
+                                  'buttonType' => 'link',
+                                  'url' => '#', //'/users/add',
+                                  //            'type'=>TbHtml::BUTTON_TYPE_SUBMIT,
+                                  'color' => TbHtml::BUTTON_COLOR_SUCCESS,
+                                  'class' => 'btn-small',
+                                  'onClick' => 'js: (function(){
+                    $("input[name=\'form_action\']").val("save");
+                    $("#yw0").submit();
+                })()'
+                              ], $option
+                  )
             );
     }
 
