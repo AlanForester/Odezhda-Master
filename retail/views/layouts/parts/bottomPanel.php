3169267fc51dc09ffd3e7eeecd9dd306234f1f63
Oleg Filimonov <olegsabian@gmail.com>
2014-01-29 12:25:55 +0400

    Refs #154 Разделены модальные окна

---
+++ retail/views/layouts/parts/bottomPanel.php
@@ -0,0 +1,72 @@
+<div class="bottom-panel">
+    <div class="bottom-panel-wrapper">
+        <div class="see-goods">
+            <a href="#" class="your-goods">вы смотрели</a>
+            <h5>5</h5>
+            <div class="your-goods-modal-wrapper">
+
+                <div class="close-modal-see-goods">
+                    X
+                </div>
+
+                <div class="your-goods-modal">
+                    <p>Вы смотрели</p>
+                    <div class="goods">
+                        <img src="/images/kofta.png" alt="" />
+                        <a href="#">Кофта (75382936)</a>
+                        <span>150 руб.</span>
+                        <button>купить</button>
+                    </div>
+                    <div class="goods">
+                        <img src="/images/kofta.png" alt="" />
+                        <a href="#">Кофта (75382936)</a>
+                        <span>150 руб.</span>
+                        <button>купить</button>
+                    </div>
+                    <div class="goods">
+                        <img src="/images/kofta.png" alt="" />
+                        <a href="#">Кофта (75382936)</a>
+                        <span>150 руб.</span>
+                        <button>купить</button>
+                    </div>
+                    <div class="goods">
+                        <img src="/images/kofta.png" alt="" />
+                        <a href="#">Кофта (75382936)</a>
+                        <span>150 руб.</span>
+                        <button>купить</button>
+                    </div>
+                    <div class="goods">
+                        <img src="/images/kofta.png" alt="" />
+                        <a href="#">Кофта (75382936)</a>
+                        <span>150 руб.</span>
+                        <button>купить</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a href="#" class="our-goods">+</a>
+        <div class="our-goods-modal">
+            <p>Ой, еще забыли предложить</p>
+            <div class="goods">
+                <img src="/images/kofta.png" alt="" />
+                <a href="#">Кофта (75382936)</a>
+                <span>150 руб.</span>
+                <button>купить</button>
+            </div>
+            <div class="goods">
+                <img src="/images/kofta.png" alt="" />
+                <a href="#">Кофта (75382936)</a>
+                <span>150 руб.</span>
+                <button>купить</button>
+            </div>
+            <div class="goods">
+                <img src="/images/kofta.png" alt="" />
+                <a href="#">Кофта (75382936)</a>
+                <span>150 руб.</span>
+                <button>купить</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<a href="#" class="scroll-top" id="up"></a>
---
+++ retail/views/layouts/parts/social.php
@@ -0,0 +1,10 @@
+<div class="fixed-social">
+    <a href="#" class="soc-btn vk"><img src="/images/l-vk.png" alt="" /></a>
+    <a href="#" class="soc-btn facebook"><img src="/images/l-facebook.png" alt="" /></a>
+    <a href="#" class="soc-btn twitter"><img src="/images/l-twitter.png" alt="" /></a>
+    <a href="#" class="soc-btn google"><img src="/images/l-google.png" alt="" /></a>
+    <a href="#" class="soc-btn odnoklassniki"><img src="/images/l-odnoklassniki.png" alt="" /></a>
+    <a href="#" class="soc-btn youtube"><img src="/images/l-youtube.png" alt="" /></a>
+    <a href="#" class="soc-btn photo"><img src="/images/l-photo.png" alt="" /></a>
+    <span></span>
+</div>
