340bf66e9db873ff8c61ce3be58d9866aeb48855
dmitri <dmitriy@maybeworks.com>
2014-02-06 11:38:56 +0200

    фронт инфо страницы

--- retail/views/site/info.php
+++ retail/views/site/info.php
@@ -4,7 +4,7 @@ Yii::app()->clientScript->registerPackage('product');
 <div class="wrapper">
 
     <div class="breadcrumbs">
-        <a href=<?php echo(Yii::app()->baseUrl)?>>Главная</a><span>/</span>
+        <a href='/'>Главная</a><span>/</span>
         <span><?php echo($infoPage['name']) ?></span>
     </div>
     <div class="karta-wrap" style="padding: 15px 20px;line-height: 20px;">
@@ -13,8 +13,5 @@ Yii::app()->clientScript->registerPackage('product');
                         <?php echo($infoPage['description']) ?>
 
     </div>
-
-            <a href="#" class="jcarousel-control-prev"></a>
-            <a href="#" class="jcarousel-control-next"></a>
 
 </div>
