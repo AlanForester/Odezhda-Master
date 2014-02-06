02ae134988cd39ad762b5dacd9f2cc98a2807cb1
Vitaliy_M <vitaliy_vm@mail.ru>
2014-02-07 02:48:21 +0600

    retail orders export command 3

--- console/commands/OrderCommand.php
+++ console/commands/OrderCommand.php
@@ -85,7 +85,69 @@ EOD;
         $this->ordersModel = new OrdersLayer();
         $this->ordersModel->setAttributes(
             [
-
+                'ur_or_fiz' => 'f',
+                'customers_id' => $this->wholesaleOrderCustomerId,
+                'customers_groups_id' => 1,
+                'customers_name' => 'Розничные заказы',
+                'customers_company' => '',
+                'customers_street_address' => '',
+                'customers_suburb' => '',
+                'customers_city' => '',
+                'customers_postcode' => '',
+                'customers_state' => '',
+                'customers_country' => '',
+                'customers_telephone' => '',
+                'customers_email_address' => '',
+                /*'customers_address_format_id' => '0',
+                'delivery_adress_id' => null,*/
+                'delivery_name' => '',
+                'delivery_lastname' => '',
+                'delivery_otchestvo' => '',
+                'delivery_pasport_seria' => '',
+                'delivery_pasport_nomer' => '',
+                'delivery_pasport_kem_vidan' => '',
+                'delivery_pasport_kogda_vidan' => '0000-00-00',
+                'delivery_company' => '',
+                'delivery_street_address' => '',
+                'delivery_suburb' => '',
+                'delivery_city' => '',
+                'delivery_postcode' => '',
+                'delivery_state' => '',
+                'delivery_country' => '',
+                //'delivery_address_format_id' => '0',
+                'billing_name' => '',
+                'billing_company' => '',
+                'billing_street_address' => '',
+                'billing_suburb' => '',
+                'billing_city' => '',
+                'billing_postcode' => '',
+                'billing_state' => '',
+                'billing_country' => '',
+                //'billing_address_format_id' => '0',
+                'payment_method' => '',
+                'payment_info' => '',
+                /*'cc_type' => '',
+                'cc_owner' => '',
+                'cc_number' => '',
+                'cc_expires' => '',
+                'last_modified' => new CDbExpression('NOW()'),
+                'date_purchased' => new CDbExpression('NOW()'),
+                'date_akt' => '0000-00-00',
+                'buh_orders_id' => '1',
+                'nomer_akt' => '0',
+                'orders_status' => '0',
+                'orders_date_finished' => new CDbExpression('NOW()'),*/
+                'currency' => 'RUR',
+                'currency_value' => '1.000000',
+                'customers_fax' => '',
+                /*'customers_referer_url' => '',
+                'shipping_module' => '',
+                'referer' => '',
+                'print_torg' => 'b',
+                'default_provider' => '0',
+                'seller_id' => '0',
+                'orders_discont' => '0',
+                'orders_discont_comment' => '',*/
             ],
             false
         );
@@ -102,7 +164,7 @@ EOD;
 
             Console::output(
                 $this->note(
-                    $updatedCount > 0 ? : 'No'
+                    $updatedCount > 0 ? $updatedCount : 'No'
                         . ' retail orders was assigned to the new wholesale order (ID '.$orderId.').'
                 )
             );
