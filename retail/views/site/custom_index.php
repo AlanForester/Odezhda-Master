620959bc5f6bb480ea27d846baabfd96b3c44724
olegblud <olegblud@gmail.com>
2014-02-09 17:05:19 +0200

    custom move to active data provider

--- retail/views/site/custom_index.php
+++ retail/views/site/custom_index.php
@@ -231,8 +231,8 @@ Yii::app()->clientScript->registerPackage('catalog');
     <div class="goods-var">
         <img src="/images/kofta.png" alt="" />
         <a href="/catalog/product/<?php echo $product->id;?>"><?php  echo $product->name.' '.$product->model; ;?></a>
-<!--        <span>--><?php //echo round($product['price']).'р'; ?><!--</span>-->
-<!--        <h5>--><?php //echo round($product['old_price']).'р'; ?><!--</h5>-->
+        <span><?php echo round($product->price).'р'; ?></span>
+        <?php if($product->old_price!=0){ ?><h5><?php echo round($product->old_price).'р'; ?></h5><?php } ?>
         <button class="m-dotted fixed-info quick-view" id="#example5" onclick="$('#exampleModalmore-goods').arcticmodal()">Быстрый просмотр</button>
         <div class="choice">
             <select>
