2e5f2b30707a563eff6e84957458b4f6b7807fd5
dmitri <dmitriy@maybeworks.com>
2014-02-11 19:02:37 +0200

    корзина

--- retail/views/layouts/parts/basket.php
+++ retail/views/layouts/parts/basket.php
@@ -1,58 +1,7 @@
-<div class="g-hidden">
-    <div class="box-modal modal-cart" id="exampleModal3">
-        <div class="box-modal_close arcticmodal-close modal-cart">X</div>
+    <div class="box-modal modal-cart" id="cart">
         <p>В вашей корзине 4 товара</p>
         <button class="clear"><b>X</b>Очистить корзину</button>
-        <p class="open-basket"><input type="checkbox"  /><span>Не открывать корзину<br /> при каждой покупке</span></p>
         <div class="cart-goods">
-            <div class="goods-one">
-                <img src="/images/kofta.png" alt="" />
-                <a href="#" class="name-goods">Кофта (75382936)</a>
-                <span class="price">2 500руб.</span>
-                <select class="sel-count">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                </select>
-                <button class="del-goods">X</button>
-            </div>
-            <div class="goods-one">
-                <img src="/images/kofta.png" alt="" />
-                <a href="#" class="name-goods">Кофта (75382936)</a>
-                <span class="price">2 500руб.</span>
-                <select class="sel-count">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                </select>
-                <button class="del-goods">X</button>
-            </div>
-            <div class="goods-one">
-                <img src="/images/kofta.png" alt="" />
-                <a href="#" class="name-goods">Кофта (75382936)</a>
-                <span class="price">2 500руб.</span>
-                <select class="sel-count">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                </select>
-                <button class="del-goods">X</button>
-            </div>
-            <div class="goods-one">
-                <img src="/images/kofta.png" alt="" />
-                <a href="#" class="name-goods">Кофта (75382936)</a>
-                <span class="price">2 500руб.</span>
-                <select class="sel-count">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                </select>
-                <button class="del-goods">X</button>
-            </div>
             <div class="goods-one">
                 <img src="/images/kofta.png" alt="" />
                 <a href="#" class="name-goods">Кофта (75382936)</a>
@@ -67,38 +16,8 @@
             </div>
         </div>
         <div class="next-sale">
-            <a href="#">Продолжить покупки</a>
             <p>Стоимость заказа</p>
             <span>5 050 руб.</span>
             <button>Оформить заказ</button>
         </div>
-        <div class="like-goods">
-            <p>Возможно Вам также понравится</p>
-            <div class="like-one">
-                <img src="/images/kofta.png" alt="" />
-                <p>Двуспальный комплект</p>
-                <a href="#">195 руб.</a>
-            </div>
-            <div class="like-one">
-                <img src="/images/kofta.png" alt="" />
-                <p>Двуспальный комплект</p>
-                <a href="#">195 руб.</a>
-            </div>
-            <div class="like-one">
-                <img src="/images/kofta.png" alt="" />
-                <p>Двуспальный комплект</p>
-                <a href="#">195 руб.</a>
-            </div>
-            <div class="like-one">
-                <img src="/images/kofta.png" alt="" />
-                <p>Двуспальный комплект</p>
-                <a href="#">195 руб.</a>
-            </div>
-            <div class="like-one">
-                <img src="/images/kofta.png" alt="" />
-                <p>Двуспальный комплект</p>
-                <a href="#">195 руб.</a>
-            </div>
-        </div>
     </div>
-</div>
--- retail/views/layouts/parts/bottomPanel.php
+++ retail/views/layouts/parts/bottomPanel.php
@@ -3,7 +3,7 @@
         <div class="see-goods">
             <a href="<?php echo $this->createUrl('cart/show')?>"
                data-options='{"width":900, "height":480, "modal": true}'
-               class="your-goods lightbox" id="openCart">
+               class="lightbox" id="openCart">
                 В корзине
             </a>
             <h5>5</h5>
