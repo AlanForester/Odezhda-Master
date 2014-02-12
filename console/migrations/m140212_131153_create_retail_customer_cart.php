8ab6a0d75eb5e7518fd3091094caea20f0fdfb1e
dmitri <dmitriy@maybeworks.com>
2014-02-12 15:23:49 +0200

    корзина

---
+++ console/migrations/m140212_131153_create_retail_customer_cart.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * TODO: Migration description
+ *
+ * @package migrations
+ */
+class m140212_131153_create_retail_customer_cart extends CDbMigration
+{
+
+	public function safeUp()
+	{
+        $this->execute("CREATE TABLE `retail_customer_cart` (
+              `customers_id` int(11) unsigned NOT NULL,
+              `products_id` int(11) unsigned NOT NULL,
+              `products_count` int(11) unsigned NOT NULL,
+              `products_size` varchar(64) NOT NULL,
+              `date_added` date NOT NULL
+              )
+        ");
+	}
+
+	public function safeDown()
+	{
+        $this->dropTable('retail_customer_cart');
+	}
+
+}
+
