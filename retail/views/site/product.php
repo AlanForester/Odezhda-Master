1b7a7e4c86706c9c84a720a40d7884cf3e7773c8
olegblud <olegblud@gmail.com>
2014-02-12 10:57:24 +0200

    full category, еще решение

--- retail/views/site/product.php
+++ retail/views/site/product.php
@@ -110,15 +110,28 @@ $this->breadcrumbs=array(
     </div>
 
     <div class="bottom-price">
+<!--        <div class="footer-social">-->
+<!--            <a href="#" class="soc-btn"><img src="/images/vk.png" alt=""/></a>-->
+<!--            <a href="#" class="soc-btn"><img src="/images/facebook.png" alt=""/></a>-->
+<!--            <a href="#" class="soc-btn"><img src="/images/twitter.png" alt=""/></a>-->
+<!--            <a href="#" class="soc-btn"><img src="/images/google.png" alt=""/></a>-->
+<!--            <a href="#" class="soc-btn"><img src="/images/odnoklassniki.png" alt=""/></a>-->
+<!--            <a href="#" class="soc-btn"><img src="/images/youtube.png" alt=""/></a>-->
+<!--            <a href="#" class="soc-btn"><img src="/images/photo.png" alt=""/></a>-->
+<!--        </div>-->
         <div class="footer-social">
-            <a href="#" class="soc-btn"><img src="/images/vk.png" alt=""/></a>
-            <a href="#" class="soc-btn"><img src="/images/facebook.png" alt=""/></a>
-            <a href="#" class="soc-btn"><img src="/images/twitter.png" alt=""/></a>
-            <a href="#" class="soc-btn"><img src="/images/google.png" alt=""/></a>
-            <a href="#" class="soc-btn"><img src="/images/odnoklassniki.png" alt=""/></a>
-            <a href="#" class="soc-btn"><img src="/images/youtube.png" alt=""/></a>
-            <a href="#" class="soc-btn"><img src="/images/photo.png" alt=""/></a>
+            <script type="text/javascript">(function() {
+                    if (window.pluso)if (typeof window.pluso.start == "function") return;
+                    if (window.ifpluso==undefined) { window.ifpluso = 1;
+                        var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+                        s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+                        s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+                        var h=d[g]('body')[0];
+                        h.appendChild(s);
+                    }})();</script>
+            <div class="pluso" data-background="transparent" data-options="medium,round,line,horizontal,nocounter,theme=06" data-services="vkontakte,facebook,google,twitter,odnoklassniki" data-url="odezhda-master.ru/" data-title="<?= Yii::app()->params['title'] ?>" data-description=""></div>
         </div>
+
         <div class="price-main">
                 	<span>
                     	  <?=FormatHelper::markup($product['price']) ?>
