72e26bd8d8858d1d37666487d1253571f94b2fcf
B@RT <bartlab@gmail.com>
2014-02-17 05:56:27 +0200

    мелочь

--- retail/views/layouts/parts/order_step1.php
+++ retail/views/layouts/parts/order_step1.php
@@ -4,8 +4,8 @@
             <p>Перепроверьте, пожалуйста, ваш телефон и выберите способ доставки</p>
             <form id="order_step1" method="post" action="javascript:void(null);">
                 <input id="phone" type="text" placeholder="Телефон" name="phone" value="<?=$customer->phone?>" required /><br />
-                <span class="delivery"><input type="radio" name="delivery" value="post" checked required /><span>Доставка почтой России</span></span>
-                <span class="delivery"><input type="radio" name="delivery" value="pickup" required /><span>Самовывоз</span></span>
+                <span class="delivery"><input type="radio" name="delivery" value="post" id="order_delivery_1" checked required /><label for="order_delivery_1">Доставка почтой России</label></span>
+                <span class="delivery"><input type="radio" name="delivery" value="pickup" id="order_delivery_2" required /><label for="order_delivery_2">Самовывоз</label></span>
                 <select id="pickup_method" style="display: none">
                     <?php foreach($deliveries as $delivery) {?>
                     <option><?=$delivery->name?></option>
