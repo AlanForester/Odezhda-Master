fd21f14649ef1d94789d15eca637839e58011c4b
dmitri <dmitriy@maybeworks.com>
2014-02-18 12:39:43 +0200

    восстановление пароля

---
+++ console/migrations/m140218_103439_create_table_retail_recovery.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Миграция создания таблицы по васстановлению пользователя(забыл пароль)
+ *
+ * @package migrations
+ */
+class m140218_103439_create_table_retail_recovery extends CDbMigration
+{
+    public function safeUp()
+    {
+        $this->execute("CREATE TABLE `retail_recovery` (
+              `customer_id` int(11) unsigned NOT NULL,
+              `hash` varchar(255) NOT NULL,
+              )
+        ");
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('retail_recovery');
+    }
+
+}
+
