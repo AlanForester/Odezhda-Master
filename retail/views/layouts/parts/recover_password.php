1b66e276eb89c8a9cb843145c1d65698e1012ae5
dmitri <dmitriy@maybeworks.com>
2014-02-18 10:55:04 +0200

    восстановление пароля

---
+++ retail/views/layouts/parts/recover_password.php
@@ -0,0 +1,38 @@
+<div class="">
+    <div class="box-modal" id="login">
+<!--        <div class="box-modal_close arcticmodal-close">X</div>-->
+        <div class="modal-login-left">
+            <p>Постоянный клиент</p>
+            <form id="log" method="post" action="javascript:void(null);">
+                <input id="email" type="text" placeholder="E-mail" name="RetailLoginForm[username]" class="required" />
+                <input id="password" type="password" placeholder="Пароль" name="RetailLoginForm[password]" class="required" />
+<!--                <div class="remember">-->
+<!--                    <input type="checkbox" name="RetailLoginForm[rememberMe]" value="1" /><span>Запомнить меня</span>-->
+<!--                </div>-->
+                <a href="#" id="reset_pass">Забыли пароль?</a>
+                <button type="submit" id="login_submit">Войти</button>
+                <button type="submit" id="login_submit_process">Отправка запроса <img src="/img/loading.gif" /></button>
+            </form>
+<!--            <div class="line"></div>-->
+<!--            <h4>Войти как пользователь социальной сети</h4>-->
+<!--            <a href="#" class="soc-btn vk"><img src="/images/vk.png" alt="" /></a>-->
+<!--            <a href="#" class="soc-btn"><img src="/images/facebook.png" alt="" /></a>-->
+<!--            <a href="#" class="soc-btn"><img src="/images/twitter.png" alt="" /></a>-->
+<!--            <a href="#" class="soc-btn"><img src="/images/google.png" alt="" /></a>-->
+<!--            <a href="#" class="soc-btn"><img src="/images/odnoklassniki.png" alt="" /></a>-->
+<!--            <div class="info">-->
+<!--                <p>Регистрация на сайте Lapana.ru дает мне дополнительные возможности.<br /><br />-->
+<!---->
+<!--                    Я могуоткладывать понравившиеся товары; отслеживать статус заказов и хранить их историю в Личном кабинете; однократно ввести адрес доставки и больше не указывать его при заказах.<br /><br />-->
+<!---->
+<!--                    А еще я получаю скидки, подарки и сюрпризы.</p>-->
+<!--            </div>-->
+        </div>
+        <div class="modal-login-right">
+            <p>Новый покупатель</p>
+            <button id="openRegForm">Регистрация</button>
+            <span id="login_error"></span>
+<!--            <img src="/images/modal-girl.png" alt="" />-->
+        </div>
+    </div>
+</div>
