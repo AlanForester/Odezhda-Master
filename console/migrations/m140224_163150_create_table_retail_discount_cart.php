2ccfa3a9f7dd4ef2375451afa2f117e1ba64f902
dmitri <dmitriy@maybeworks.com>
2014-02-24 18:50:59 +0200

    коды дискаунт таблица

--- console/migrations/m140224_163150_create_table_retail_discount_cart.php
+++ console/migrations/m140224_163150_create_table_retail_discount_cart.php
@@ -10,10 +10,11 @@ class m140224_163150_create_table_retail_discount_cart extends CDbMigration
     public function up()
 	{
         $this->execute("CREATE TABLE `retail_discount_cart` (
-              `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT,
+              `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
               `code` varchar(10) NOT NULL COMMENT  'Уникальный код',
               `status` int(2) NOT NULL DEFAULT '0' COMMENT  'Статус кода',
               `activated` DATE DEFAULT NULL COMMENT  'Дата активации',
+              PRIMARY KEY (`id`),
               UNIQUE KEY `code` (`code`)
               )
         ");
