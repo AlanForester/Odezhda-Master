c2babe7debb5648b4481e671951c2c7977db000b
Zimovid <arxanjel@ya.ru>
2014-02-25 18:30:25 +0200

    Добавление кнопок

---
+++ backend/views/wholesale_delivery/index.php
@@ -0,0 +1,7 @@
+<?php
+// кнопки страницы
+$this->pageButton = [
+    BackendPageButtons::add("/wholesale_delivery/add"),
+    BackendPageButtons::remove("/wholesale_delivery/mass"),
+    BackendPageButtons::mass("/wholesale_delivery/mass")
+];
