716da5a60a1d49dbf85ac43ab29a9b3c91796854
Zimovid <arxanjel@ya.ru>
2014-02-25 17:29:27 +0200

    Создание контроллера для Оптовые заказы

---
+++ backend/controllers/WholesaleDeliveryController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zimovid
+ * Date: 25.02.14
+ * Time: 18:10
+ */
+class WholesaleDeliveryController extends BackendController {
+
+    public function actionIndex()
+    {
+        $this->render('index');
+    }
+
+
+}
