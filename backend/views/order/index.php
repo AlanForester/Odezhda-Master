8bb14be8af112ec622b9f45da71f812aaf0c0174
Zimovid <arxanjel@ya.ru>
2014-02-26 15:41:43 +0200

    Вывод views order

--- backend/views/order/index.php
+++ backend/views/order/index.php
@@ -5,3 +5,42 @@ $this->pageButton = [
     BackendPageButtons::remove("/order/mass"),
     BackendPageButtons::mass("/order/mass")
 ];
+// таблица
+$this->widget(
+    'backend.widgets.Grid',
+    [
+        'submenu' => BackendSubMenu::orders(),
+        'filter' => [ ],
+
+        'order' => [
+            //'active' => $criteria['order_field'],
+            'fields' => [
+                'firstname' => 'Имя',
+                'lastname' => 'Фамилия',
+                'email' => 'E-Mail',
+                'group_id' => 'Группа',
+                'logdate' => 'Последний визит',
+                'id' => 'ID',
+                'customers_name' => 'Имя',
+            ],
+            //'direct' => $criteria['order_direct']
+
+        ],
+
+        'pageSize' => $criteria['page_size'],
+
+        'textSearch' => $criteria['text_search'],
+
+        'dataProvider' => $gridDataProvider,
+
+        'gridColumns' => [ ],
+
+        'gridButtonsUrl' => [
+            'edit' => 'Yii::app()->createUrl("/orders/edit", array("id"=>$data["id"]))',
+            'delete' => 'Yii::app()->createUrl("/orders/delete", array("id"=>$data["id"]))',
+        ]
+
+
+    ]
+
+);
