3c0475eae75fba59068fedb5ab6240b2a155fbdd
Zimovid <arxanjel@ya.ru>
2014-02-26 15:42:35 +0200

    Прописал контроллер для orders

---
+++ backend/controllers/OrdersController.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Class OrderController
+ */
+class OrdersController extends BackendController
+{
+
+    public $pageTitle = 'Оптовые заказы: список';
+    public $pageButton = [];
+    public $model;
+
+    public function actionIndex(){
+        $criteria = [
+            'text_search' => [
+                'value' => $this->userStateParam('text_search'),
+            ],
+            'filters' => $this->userStateParam('filters'),
+            'order' => [
+                'field' => $this->userStateParam('order_field'),
+                'direction' => $this->userStateParam('order_direct'),
+            ],
+            'page_size' => $this->userStateParam('page_size', CPagination::DEFAULT_PAGE_SIZE)
+        ];
+        // получение данных
+        $this->model = new OrdersModel();
+        $gridDataProvider = $this->model->getDataProvider($criteria);
+
+        $groups_model = new GroupsModel();
+        foreach ($groups_model->getList() as $g) {
+            $groups[$g['id']] = $g['name'];
+        }
+
+        $this->render('index', compact('page_size', 'criteria', 'gridDataProvider'));
+    }
+}
