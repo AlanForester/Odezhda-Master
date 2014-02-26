57a5eb5508d73e823a7370e85735a5b629bfa90d
Zimovid <arxanjel@ya.ru>
2014-02-26 12:04:29 +0200

    прописал ControllerMAP для order

--- backend/views/wholesale_delivery/edit.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Zimovid
- * Date: 25.02.14
- * Time: 18:04
- */ 
--- backend/views/wholesale_delivery/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// кнопки страницы
-$this->pageButton = [
-    BackendPageButtons::add("/wholesale_delivery/add"),
-    BackendPageButtons::remove("/wholesale_delivery/mass"),
-    BackendPageButtons::mass("/wholesale_delivery/mass")
-];
