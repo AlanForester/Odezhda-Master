e7f0e9442230920f28d48d5251d766288fae54fd
Vitaliy_M <vitaliy_vm@mail.ru>
2014-02-28 02:42:44 +0600

    widget: scripts

--- retail/controllers/WidgetController.php
+++ retail/controllers/WidgetController.php
@@ -2,12 +2,14 @@
 
 class WidgetController extends RetailController {
 
-    public function actionMain() {
+    public function actionIndex() {
         $callback = Yii::app()->request->getQuery('callback', 'LapanaWidget.initCallback');
 
         //выбор товара дня из категории "акции" 1435 или Спецпредложение 590
         $category = 1435;
         $dayProducts = ShopProductsHelper::widgetDayProducts($category);
+
+        //print_r($dayProducts);die();
 
         $response = [
             'html' => $this->renderPartial('main', compact('dayProducts'), true)
--- retail/www/js/widget.js
+++ retail/www/js/widget.js
@@ -3,22 +3,36 @@ var LapanaWidget = {
     baseURL: '/widget/',    //'http://lapana.ru/widget/',
 
     init: function (params) {
+        this.load('index', 'LapanaWidget.initCallback');
 
-        if(params.id != undefined) {
-            this.load('main', 'LapanaWidget.initCallback');
+        /*if(params.id != undefined) {
 
         } else {
             alert('Идентификатор контейнера для загрузки виджета Lapana не указан');
-        }
+        }*/
 
     },
 
     initCallback: function (response) {
-        //alert(response.html);
+        this.registerStyle();
+        this.addWidget(response.html);
+    },
+
+    addWidget: function (html) {
         var div = document.createElement("div");
         div.className = 'lapana-widget';
-        div.innerHTML = response.html;
+        div.innerHTML = html;
         document.body.appendChild(div);
+    },
+
+    registerStyle: function () {
+        var headID = document.getElementsByTagName('head')[0];
+        var cssNode = document.createElement('link');
+        cssNode.type = 'text/css';
+        cssNode.rel = 'stylesheet';
+        cssNode.href = this.baseURL + 'css/widget.css';
+        cssNode.media = 'screen';
+        headID.appendChild(cssNode);
     },
 
     slideUp: function () {
@@ -55,5 +69,13 @@ var LapanaWidget = {
 
     setLoaded: function (element) {
         element.loaded = 1;
+    },
+
+    bind: function (element, eventName, fn) {
+        if (document.getElementById && document.getElementsByTagName) {
+            if (window.addEventListener) element.addEventListener(eventName, fn, false);
+            else if (window.attachEvent) element.attachEvent('on'+eventName, fn);
+        }
     }
+
 };
